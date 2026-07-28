c8c71e0624574babb6dace5dca128cd0fb10b2fd
joelleal851 <joel.leal.junior@escola.pr.gov.br>
2026-07-28 18:10:24 -0300

    1

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarCuriosidade() {
+
+    const resultado = document.getElementById("resultado");
+
+    resultado.textContent =
+        "🔎 [EDITAR AQUI] Coloque uma curiosidade sobre o tema da equipe.";
+
+}
